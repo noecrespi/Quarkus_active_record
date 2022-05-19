0177c425bd9599a6db88f152ca7093c8ac174ddb
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-19 16:26:05 +0200

    [:sparkles:] add class PersonaService

---
+++ src/main/java/edu/poniperro/quickstart/PersonaService.java
@@ -0,0 +1,33 @@
+package edu.poniperro.quickstart;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Es para inyectar cosas
+@ApplicationScoped
+public class PersonaService {
+
+    public Set<Persona> getAll() {
+        Stream<Persona> personas = Persona.streamAll();
+        // .collect() = transformar un stream a un tipo de lista.
+        return personas.collect(Collectors.toSet());
+    }
+
+    public Optional<Persona> getById(Long idPersona) {
+        // mirar repo de Victor
+        return Persona.find("id", idPersona).firstResultOptional();
+    }
+
+    public void deletePersona(String nombre) {
+        Persona persona1 = Persona.find("nombre", nombre).firstResult();
+        persona1.delete();
+    }
+
+    public void postPersona (Persona persona){
+        persona.persist();
+    }
+
+}
